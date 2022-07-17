ee3e9212378e86c0ddea43e695df2bffb4f25ddb
codeservr <bosstoner@gmail.com>
2022-07-17 17:35:24 +0000

    ex10

---
+++ hardway/ex10.py
@@ -0,0 +1,32 @@
+tabbyCat = "\tI'm tabbed in."
+persianCat = "I'm split\non a line."
+backslashCat = "I'm \\ a \\ cat."
+fatCat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabbyCat)
+print(persianCat)
+print(backslashCat)
+print(fatCat)
+
+
+# Escape - What it does.
+# \\ Backslash (\)
+# \' Single-quote (')
+# \" Double-quote (")
+# \a ASCII bell (BEL)
+# \b ASCII backspace (BS)
+# \f ASCII formfeed (FF)
+# \n ASCII linefeed (LF)
+# \N{name} Character named name in the Unicode database (Unicode only)
+# \r Carriage return (CR)
+# \t Horizontal tab (TAB)
+# \uxxxx Character with 16-bit hex value xxxx
+# \Uxxxxxxxx Character with 32-bit hex value xxxxxxxx
+# \v ASCII vertical tab (VT)
+# \000 Character with octal value 000
+# \xhh Character with hex value hh
---
+++ hardway/ex5.py
@@ -0,0 +1,15 @@
+myName = 'Ben Klein'
+myAge = 30 #soon
+myHeight = 185
+myWeight = 124
+myEyes = 'green'
+myTeeth = 'White'
+myHair = 'Black'
+
+print(f"Let's talk about {myName}.")
+print(f"He's {myHeight} cms tall.")
+print(f"He's {myWeight} kgs heavy.")
+print("yeah..")
+
+total = myAge + myHeight + myWeight
+print(f"If I add {myAge}, {myHeight}, and {myWeight} I get {total}.")
