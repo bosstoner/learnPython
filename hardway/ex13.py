f03bfade7145cf630f4b3e546bff694acf724136
codeservr <bosstoner@gmail.com>
2022-07-18 19:29:10 +0000

    ex15

---
+++ hardway/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+script, first, second, third = argv
+
+# the first argument after the 'python3' command is the name of the script 
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+input("Your third variable is: ")
